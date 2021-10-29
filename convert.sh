9b29256ea057e770009e5f11a93dcc1146105762
taneishi <taneishi@users.noreply.github.com>
2021-10-28 21:44:52 -0700

    appended pip upgrade process.

--- convert.sh
+++ convert.sh
@@ -2,7 +2,11 @@
 
 #python3 -m venv openvino
 source openvino/bin/activate
-#pip install openvino_dev onnx==1.8.1
+#pip install --upgrade pip
+#pip install openvino_dev torchvision onnx==1.8.1
 
 #python mnist.py
+python infer.py
+
 mo --input_model model/convnet.onnx --output_dir model
+python infer.py --mode openvino
