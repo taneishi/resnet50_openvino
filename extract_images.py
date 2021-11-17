d3acc9774e033df8fcfa0ede4ed9002e50214eba
taneishi <taneishi@users.noreply.github.com>
2021-11-17 16:07:57 +0900

    changed to use torchvision dataset to generate test dataset.

--- extract_images.py
+++ extract_images.py
@@ -1,35 +1,38 @@
+import torchvision
+import torch
 import numpy as np
 import cv2
 import os
 
 def main():
+    transform = torchvision.transforms.ToTensor()
+
     data_dir = 'data/MNIST'
     image_dir = '%s/images' % data_dir
     if not os.path.exists(image_dir):
         os.makedirs(image_dir, exist_ok=True)
 
-    train_image = 'raw/train-images-idx3-ubyte'
-    train_label = 'raw/train-labels-idx1-ubyte'
-    test_image = 'raw/t10k-images-idx3-ubyte'
-    test_label = 'raw/t10k-labels-idx1-ubyte'
+    test_dataset = torchvision.datasets.MNIST(
+            root='data',
+            train=False,
+            transform=transform)
 
-    with open(os.path.join(data_dir, test_image), 'rb') as f:
-        images = f.read()
-
-    images = [d for d in images[16:]]
-    images = np.array(images, dtype=np.uint8)
-    images = images.reshape((-1, 28, 28))
-
-    for index, image in enumerate(images):
-        cv2.imwrite(os.path.join(image_dir, '%d.png' % (index)), image)
-    
-    with open(os.path.join(data_dir, test_label), 'rb') as f:
-        labels = f.read()
-
-    labels = labels[8:]
+    test_loader = torch.utils.data.DataLoader(
+            dataset=test_dataset,
+            batch_size=1,
+            shuffle=False,
+            num_workers=0,
+            pin_memory=True,
+            drop_last=True)
 
     with open('%s/val.txt' % data_dir, 'w') as f:
-        for label in labels:
+        for index, (image, label) in enumerate(test_loader):
+
+            image = np.array(image * 256, dtype=np.uint8)
+            image = image.reshape((28, 28))
+
+            cv2.imwrite(os.path.join(image_dir, '%d.png' % (index)), image)
+
             f.write('%d\n' % (label))
 
 if __name__ == '__main__':
