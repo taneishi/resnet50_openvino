9c025f7f9c5f073229df783bec20e223223af9af
taneishi <taneishi@users.noreply.github.com>
2021-11-17 11:07:54 +0900

    appended test inference in each epoch.

--- mnist.py
+++ mnist.py
@@ -1,29 +1,23 @@
 import torchvision
-import torchvision.transforms as transforms
 import torch
 import torch.nn as nn
-import timeit
 import argparse
+import timeit
 import os
 
 from model import ConvNet
 
 def main(args):
+    torch.manual_seed(123)
     device = torch.device('cpu')
 
-    net = ConvNet()
-    net = net.to(device)
-
-    # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss()
-    criterion = criterion.to(device)
-    optimizer = torch.optim.SGD(net.parameters(), 1e-4)
+    transform = torchvision.transforms.ToTensor()
 
     # Data loading code
     train_dataset = torchvision.datasets.MNIST(
             root=args.data_dir,
             train=True,
-            transform=transforms.ToTensor(),
+            transform=transform,
             download=True)
 
     train_loader = torch.utils.data.DataLoader(
@@ -33,11 +27,31 @@ def main(args):
             num_workers=0,
             pin_memory=True)
 
+    test_dataset = torchvision.datasets.MNIST(
+            root=args.data_dir,
+            train=False,
+            transform=transform)
+
+    test_loader = torch.utils.data.DataLoader(
+            dataset=test_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=0,
+            pin_memory=True)
+
+    net = ConvNet()
+    net = net.to(device)
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), 1e-4)
+
     for epoch in range(args.epochs):
-        start = timeit.default_timer()
-        for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
-            labels = labels.to(device)
+        epoch_start = timeit.default_timer()
+
+        net.train()
+        for index, (images, labels) in enumerate(train_loader):
+            print('\rbatch %d/%d' % (index, len(train_loader)), end='')
 
             # Forward pass
             outputs = net(images)
@@ -48,7 +62,18 @@ def main(args):
             loss.backward()
             optimizer.step()
 
-        print('epoch [% 4d/% 4d], train loss %6.4f, %5.3fsec' % (epoch+1, args.epochs, loss.item(), timeit.default_timer() - start))
+        print('\repoch % 5d train loss %6.4f' % (epoch+1, loss.item()), end='')
+
+        net.eval()
+        test_loss = 0
+        for images, labels in test_loader:
+            with torch.no_grad():
+                output = net(images)
+            test_loss += criterion(output, labels).item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+
+        print(' test average loss %6.4f' % (test_loss / len(test_loader)), end='')
+        print(' %5.3fsec' % (timeit.default_timer() - epoch_start))
 
     os.makedirs('model', exist_ok=True)
     torch.save(net.state_dict(), 'model/convnet.pth')
@@ -57,15 +82,10 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_threads', default=None, type=int, help='number of threads')
     parser.add_argument('--epochs', default=5, type=int, metavar='N', help='number of total epochs to run')
     parser.add_argument('--batch_size', default=96, type=int, help='batch size')
     parser.add_argument('--data_dir', default='data', type=str)
     args = parser.parse_args()
-    if not args.num_threads:
-        args.num_threads = torch.get_num_threads()
-    else:
-        torch.set_num_threads(args.num_threads)
     print(vars(args))
 
     main(args)
