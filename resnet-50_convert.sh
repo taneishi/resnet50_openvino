01e30dc1b965d317ab29c395265b7f600b4ce866
taneishi <taneishi@users.noreply.github.com>
2021-11-29 14:19:10 +0900

    removed conversion script for resnet-50.

--- resnet-50_convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#source openvino/bin/activate
-
-omz_downloader --name resnet-50-pytorch
-
-omz_converter --name resnet-50-pytorch
-
-convert_annotation cifar -o annotation --data_batch_file data/cifar-10-batches-py/test_batch
