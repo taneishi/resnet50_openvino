5c690957bcbe90ef05b9643ef258004db9dfb271
taneishi <taneishi@users.noreply.github.com>
2022-03-29 19:00:19 +0900

    set dfault number of cores.

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#PBS -l nodes=2:gold6330
+#PBS -N torch_ddp
+#PBS -j oe
+#PBS -o output_${PBS_NUM_NODES}.log
 
-pip install -r requirements.txt
+if [ ${PBS_O_WORKDIR} ]; then
+    cd ${PBS_O_WORKDIR}
+fi
 
 if [ ! -d images ]; then
     git clone https://github.com/EliSchwartz/imagenet-sample-images images
@@ -9,6 +15,16 @@ fi
 if [ ! -f imagenet_classes.txt ]; then
     wget -c https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt
 fi
+
+CPUS=2
+CORES=24
+TOTAL_CORES=$((${CPUS}*${CORES}))
+
+echo "CPUS=${CPUS} CORES=${CORES} TOTAL_CORES=${TOTAL_CORES}"
+export OMP_NUM_THREADS=${TOTAL_CORES}
+export KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+
+pip install -q -r requirements.txt
 
 if [ ! -f model/resnet-50.onnx ]; then
     python export_onnx.py
