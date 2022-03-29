3cb5658a598e421c35d64e713e9efe30a7b1ce40
taneishi <taneishi@users.noreply.github.com>
2022-03-29 19:06:59 +0900

    removed quiet flag of pip.

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ echo "CPUS=${CPUS} CORES=${CORES} TOTAL_CORES=${TOTAL_CORES}"
 export OMP_NUM_THREADS=${TOTAL_CORES}
 export KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
 
-pip install -q -r requirements.txt
+pip install -r requirements.txt
 
 if [ ! -f model/resnet-50.onnx ]; then
     python export_onnx.py
